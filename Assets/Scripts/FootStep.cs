9b662842af705e0e2a346da156ea4d0c561d5aa6
Evgenii Kucheriavyi <e.kucheriavii@gmail.com>
2020-01-04 23:15:58 +0300

    Added footsteps

---
+++ Assets/Scripts/FootStep.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class FootStep : MonoBehaviour
+{
+
+	AudioSource audio;
+
+	public void OnTriggerEnter(Collider other)
+	{
+		if(other.transform.tag == "Ground")
+		{
+			audio.Play();
+		}
+	}
+
+	public void OnCollisionEnter(Collision other)
+	{
+		if(other.transform.tag == "Ground")
+		{
+			audio.Play();
+		}
+	}
+
+	void Start()
+	{
+		audio = GetComponent<AudioSource>();
+	}
+}
